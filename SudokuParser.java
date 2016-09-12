122b02100769647f39e7db5ba2782ef86641f079
matthino <matthino@gmail.com>
2016-09-11 18:45:27 -0700

    Added parser so that we can read and write sudoku puzzles to files. added some tests for that.

---
+++ SudokuParser.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+public class SudokuParser {
+
+  private static final String LINE_SEPARATOR = "+=======================+";
+  private static final int REGION_SIZE = 3;
+
+  // takes a sudoku puzzle and writes it to a file
+  public static void writeSudoku(Square[] puzzle, String fileName) throws IOException {
+    try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+      for(int square_no = 0; square_no < puzzle.length; square_no++ ){
+
+        // write a new line if we've written a full row
+        if(square_no%Square.NUM_SQUARES_ROW_COL==0 && square_no != 0) {
+          bw.write("| ");
+          bw.newLine();
+        }
+
+        // write a horizontal bar to separate regions
+        if(square_no%(Square.NUM_SQUARES_ROW_COL * REGION_SIZE) == 0) {
+          bw.write(LINE_SEPARATOR);
+          bw.newLine();
+        }
+
+        // write a pipe to separate the regions
+        if(square_no%REGION_SIZE == 0) {
+          bw.write("| ");
+        }
+
+        String numberToWrite = Integer.toString(puzzle[square_no].get_value());
+        if(puzzle[square_no].get_value() == Square.UNKNOWN) {
+          bw.write("- ");
+        } else {
+          bw.write(numberToWrite + " ");
+        }
+      }
+
+      bw.write("| ");
+      bw.newLine();
+      bw.write(LINE_SEPARATOR);
+      bw.close();
+    }
+  }
+
+  // Reads in a sudoku grid from a text file and converts it to a square array
+  public static Square[] parseSudoku(String fileName) throws IOException {
+
+    List<Square> squareList = new ArrayList<Square>();
+
+    // open the file and read line by line
+    // skip lines that start with the '+' character
+    // remove the pipes and convert the '-' to '0'
+    // split the string by spaces then get each int value for the square
+    try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+      int square_no = 0;
+      for(String line; (line = br.readLine()) != null; ) {
+          // if it's a non-spacing line, then convert to squares
+          if(line.length() > 0 && (line.charAt(0) != '+')) {
+            line = line.replace("|", "").replace("-", "0");
+            String[] values = line.split(" ");
+
+            for(int i=0; i < values.length; i++) {
+              if(values[i].length() > 0) {
+                try {
+                  squareList.add(new Square(square_no, Integer.parseInt(values[i])));
+                } catch (NumberFormatException nfe) {
+                  System.out.println("Error in parsing Sudoku from file" + nfe);
+                } finally {
+                  square_no++;
+                }
+              }
+            }
+          }
+      }
+
+      // after parsing the file, return the squareList
+      return squareList.toArray(new Square[squareList.size()]);
+    }
+  }
+
+  private static void printStringArray(String[] strings) {
+    for(String string: strings) {
+      System.out.print(string + " " );
+    }
+    System.out.print("\n");
+  }
+
+}
--- SudokuSolverTest.java
+++ SudokuSolverTest.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class SudokuSolverTest {
 
@@ -14,7 +15,7 @@ public class SudokuSolverTest {
                                            0,0,3, 0,2,0, 5,0,8,
                                            0,2,0, 0,8,0, 0,7,0,
                                            8,0,0, 0,0,3, 6,0,0 };
-                                           
+
    // http://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html
   private static int[] ridiculousPuzzle = {8,0,0, 0,0,0, 0,0,0,
                                            0,0,3, 6,0,0, 0,0,0,
@@ -28,21 +29,48 @@ public class SudokuSolverTest {
                                            0,0,8, 5,0,0, 0,1,0,
                                            0,9,0, 0,0,0, 4,0,0 };
 
+  private static int[] ridiculousSolved = {8,1,2, 7,5,3, 6,4,9,
+                                           9,4,3, 6,8,2, 1,7,5,
+                                           6,7,5, 4,9,1, 2,8,3,
+
+                                           1,5,4, 2,3,7, 8,9,6,
+                                           3,6,9, 8,4,5, 7,2,1,
+                                           2,8,7, 1,6,9, 5,3,4,
+
+                                           5,2,1, 9,7,4, 3,6,8,
+                                           4,3,8, 5,2,6, 9,1,7,
+                                           7,9,6, 3,1,8, 4,5,2};
+
 
   public static void main(String [] args) {
-    System.out.println("Tests for SudokuSolver:");
-    solvePuzzleTimed(convertArrayToSquareArray(kindaHardPuzzle));
-    solvePuzzleTimed(convertArrayToSquareArray(ridiculousPuzzle));
+    try {
+      System.out.println("Tests for SudokuSolver:");
+      solvePuzzleTimed(convertArrayToSquareArray(kindaHardPuzzle), "kindaHardPuzzle_solved.txt");
+      solvePuzzleTimed(convertArrayToSquareArray(ridiculousPuzzle), "ridiculousPuzzle_solved.txt");
+
+      System.out.println("Testing Parser:");
+
+      Square[] result = SudokuParser.parseSudoku("test_puzzle.txt");
+      SudokuParser.writeSudoku(result, "test_puzzle_output.txt");
+      SudokuSolver solver = new SudokuSolver();
+
+      Optional<Square[]> solved = solver.solve_puzzle(result);
+      if(solved.isPresent()) {
+          SudokuParser.writeSudoku(solved.get(), "test_puzzle_solved.txt");
+      }
+    } catch (IOException ioe) {
+      System.out.println("Uh oh, an error occured while parsing ");
+    }
   }
 
-  public static void solvePuzzleTimed(Square[] puzzle) {
+  public static void solvePuzzleTimed(Square[] puzzle, String outputFileName) throws IOException {
     SudokuSolver solver = new SudokuSolver();
 
     long startTime = System.currentTimeMillis();
     Optional<Square[]> solved = solver.solve_puzzle(puzzle);
     long endTime = System.currentTimeMillis();
     if(solved.isPresent()) {
-      print_sudoku_puzzle(solved.get());
+      SudokuParser.writeSudoku(solved.get(), outputFileName);
     }
     System.out.println("Solving took " + (endTime - startTime) + " milliseconds\n");
   }
