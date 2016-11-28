26421f31ce2c4d7ee4f3bf229a61b37139c77fa6
Christine Chen <chenc17@up.edu>
2016-11-27 18:39:31 -0800

    Added generator to generate unsolved Sudoku puzzles.

---
+++ prolog/generator.pl
@@ -0,0 +1,118 @@
+/*generator.pl writes an unsolved sudoku puzzle to a file*/
+
+/*import in the solver.pl file and some libraries*/
+:- consult('solver.pl').
+:- use_module(library(clpfd)).
+:- use_module(library(random)).
+
+/*initial empty Sudoku grid*/
+problem(2, [[_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_],
+          [_,_,_,_,_,_,_,_,_]]).
+
+
+/*generator*/
+generator :-
+
+  /*get empty Sudoku grid*/
+  problem(2,Rows),
+
+  /*get unique numbers in the range 1-9*/
+  randseq(5, 9, Seq),
+
+  /*get the first row of the empty sudoku grid*/
+  nth0(0, Rows, First_Row, Others),
+
+  /*put the 1st element of Seq into the first spot in First_Row*/
+  nth0(0, Seq, Val1, Rest1),
+  setElement(First_Row, 0, Val1, A),
+  %write(A),
+
+  /*2nd element*/
+  nth0(0, Rest1, Val2, Rest2),
+  setElement(A, 1, Val2, B),
+  %write(B),
+
+  /*3rd element*/
+  nth0(0, Rest2, Val3, Rest3),
+  setElement(B, 2, Val3, C),
+  %write(C),
+
+  /*4th element*/
+  nth0(0, Rest3, Val4, Rest4),
+  setElement(C, 3, Val4, D),
+  %write(D),
+
+  /*5th element*/
+  nth0(0, Rest4, Val5, Rest5),
+  setElement(D, 4, Val5, E),
+  %write(E),
+
+  /*set the first row of the sudoku grid to be the newly modified row*/
+  setElement(Rows,0,E,Sudoku_Grid),
+  %write(Sudoku_Grid),
+
+  /*solve the sudoku grid with 5 values to get a fully solved grid*/
+  sudoku(Sudoku_Grid),
+  %write(Sudoku_Grid),
+
+  /*list of lists to single list of values*/
+  flatten(Sudoku_Grid,All_Vals),
+  %write(All_Vals),
+
+  /*get a list containing 50 random values between 1 and 81...these
+  values correspond with the indices of the values in All_Vals that will
+  be replaced with the anonymous variable*/
+  randset(40, 81, Replace_Indices),
+  %write(Replace_Indices), nl,
+
+  /*now actually replace values in All_Vals accordingly*/
+  create_unsolved(All_Vals, Replace_Indices, Result),
+  %write(Result),
+
+  /*change the list back to a list of lists*/
+  lst_2_lst_of_lst(Result, 9, List_Of_Lists),
+  maplist(writeln, List_Of_Lists), nl,
+
+  /*check whether or not the puzzle is solveable*/
+  (sudoku(List_Of_Lists) ->
+  maplist(writeln, List_Of_Lists), nl
+  ;
+  write("Calling generator again!"), nl,
+  generator).
+
+/*takes a list of sudoku values and replaces all values specified by middle list
+with the anonymous variable*/
+create_unsolved(Result,[],Result).
+create_unsolved(Sudoku_Vals, [Idx_Replace|Rest], Result) :-
+  setElement(Sudoku_Vals, Idx_Replace, _, New_Result),
+  create_unsolved(New_Result, Rest, Result).
+
+/*clauses courtesy of http://www.tek-tips.com/viewthread.cfm?qid=1504821
+they collectively turn a list into a list of lists where each sublist is of length
+N*/
+lst_2_lst_of_lst([], _N, []).
+
+lst_2_lst_of_lst(L, N, LL) :-
+    lst_2_lst_of_lst_helper(L, 1, N, LL).
+
+lst_2_lst_of_lst_helper([H|T], N, N, [[H]|LL]):-
+    lst_2_lst_of_lst(T, N, LL).
+
+lst_2_lst_of_lst_helper([H|T], N1 , N, [[H|TMP]|LL]):-
+    N2 is N1 + 1,
+    lst_2_lst_of_lst_helper(T, N2 , N, [TMP| LL]).
+
+/* sets the Ith element of a list to Value and
+stores the output in the last parameter */
+setElement([_|Rest], 0, Value, [Value|Rest]).
+
+setElement([First|Rest], I, Value, [First|End]):-
+ I > 0, Next is I-1,
+ setElement(Rest, Next, Value, End).
--- prolog/solver.pl
+++ prolog/solver.pl
@@ -1,10 +1,11 @@
 
 /*http://www.swi-prolog.org/pldoc/man?predicate=transpose/2*/
+:- use_module(library(clpfd)).
 
 sudoku(Rows) :-
   append(Rows, Vs), Vs ins 1..9,
   maplist(all_distinct, Rows),
-  transpose(Rows, Columns),
+  rows_to_cols(Rows, Columns),
   maplist(all_distinct, Columns),
   Rows = [A,B,C,D,E,F,G,H,I],
   blocks(A, B, C), blocks(D, E, F), blocks(G, H, I),
@@ -17,20 +18,20 @@ blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
 
 
 /*http://stackoverflow.com/questions/4280986/how-to-transpose-a-matrix-in-prolog*/
-transpose([],[]).
+rows_to_cols([],[]).
 
 /*Isolate the first row and specify the result desired (columns)*/
-transpose([FIRST_ROW|OTHER_ROWS], COLUMNS) :-
-  transpose(FIRST_ROW, [FIRST_ROW|OTHER_ROWS], COLUMNS).
+rows_to_cols([FIRST_ROW|OTHER_ROWS], COLUMNS) :-
+  rows_to_cols(FIRST_ROW, [FIRST_ROW|OTHER_ROWS], COLUMNS).
 
-transpose([],_,[]).
+rows_to_cols([],_,[]).
 /*Isolate the tail of the first row, all rows, and the first column*/
-transpose([_|REST_FR],ALL_ROWS,[COL_1|OTHER_COLS]) :-
+rows_to_cols([_|REST_FR],ALL_ROWS,[COL_1|OTHER_COLS]) :-
   /*call lists_firsts_rests with all the rows, the first column, and specify a variable to hold the result*/
   lists_firsts_rests(ALL_ROWS,COL_1,RESULT),
   /*recurse with the rest of the first row, the result that came back in from lists_firsts_rests
   (all rows with the same amount of elements shaved off the front of each row), and the rest of the ultimate columns*/
-  transpose(REST_FR, RESULT, OTHER_COLS).
+  rows_to_cols(REST_FR, RESULT, OTHER_COLS).
 
 lists_firsts_rests([],[],[]).
 /*Isolate the head of the first row, make that the head of the first column, put the tail of the first row in the result variable*/
