4e81353210df3fbd7822ebcb2c89bd6df927b52c
Christine Chen <chenc17@up.edu>
2016-12-05 16:21:43 -0800

    Modified generator.py so that the generate_sudoku_puzzle function returns an unsolved puzzle AND its corresponding solution.

--- python/generator.py
+++ python/generator.py
@@ -6,12 +6,16 @@ EASY_VALS_TO_REMOVE = 30
 MED_VALS_TO_REMOVE = 50
 HARD_VALS_TO_REMOVE = 60
 
+#generator
+#created: 12/4/16
 
+#given a difficulty number that indicates how many values to remove from the sudoku puzzle,
+#generate an unsolved sudoku puzzle and its corresponding solution
 def generate_sudoku_puzzle(Diff_Num):
     
 
     while True:
-        print("generating puzzle!")
+        
         #blank puzzle
         init_puzzle = [[0 for x in range(solver.NUM_SQ_ROW_COL)] for y in range(solver.NUM_SQ_ROW_COL)]
     
@@ -29,9 +33,9 @@ def generate_sudoku_puzzle(Diff_Num):
             if(sudoku_puzzle!=None):
                 break
         
-        print("solved puzzle:")
-        solver.printPuzzle(sudoku_puzzle)
-        print("\n")
+#        print("solved puzzle:")
+#        solver.printPuzzle(sudoku_puzzle)
+#        print("\n")
         
         #pick NUM_VALS_TO_REMOVE indices and set values corresponding to those 
         #indices to 0 
@@ -40,29 +44,34 @@ def generate_sudoku_puzzle(Diff_Num):
         for i in indices_selected:
             sudoku_puzzle[solver.getRowIndex(i)][solver.getColIndex(i)] = solver.UNKNOWN
 
-        print("unsolved puzzle:")
-        solver.printPuzzle(sudoku_puzzle)
-        print("\n")
+#        print("unsolved puzzle:")
+#        solver.printPuzzle(sudoku_puzzle)
+#        print("\n")
     
         sudoku_puzzle_final = solver.createSolved(sudoku_puzzle,0)
+        s_p_solved = solver.copyPuzzle(sudoku_puzzle_final)
         
     
-        
         if(sudoku_puzzle_final != None):
            
-            print("final solved puzzle:")
-            solver.printPuzzle(sudoku_puzzle_final)
-            print("\n")
             
             #take out values again
             for i in indices_selected:
                 sudoku_puzzle_final[solver.getRowIndex(i)][solver.getColIndex(i)] = solver.UNKNOWN
-            return sudoku_puzzle_final
+            return [s_p_solved, sudoku_puzzle_final]
        
-            
-   
-solver.printPuzzle(generate_sudoku_puzzle(HARD_VALS_TO_REMOVE))
-print("FINAL UNSOLVED PUZZLE")
+ #test function           
+def test_generator():
+    
+    puzzles_generated = generate_sudoku_puzzle(EASY_VALS_TO_REMOVE)
+    
+    print("FINAL SOLVED PUZZLE")
+    solver.printPuzzle(puzzles_generated[0])
+    print("\n")
+    
+    print("FINAL UNSOLVED PUZZLE")
+    solver.printPuzzle(puzzles_generated[1])
+    print("\n")
     
     
     
